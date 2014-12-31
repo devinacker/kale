7e09b1c97775cd9abd1652a59e4371be7c6be6ee
devinacker <d@revenant1.net>
2014-12-31 00:23:25 -0500

    bump version number also

--- src/version.h
+++ src/version.h
@@ -8,11 +8,11 @@
 
 #define INFO_TITLE  "KALE"
 #define INFO_MYNAME "Devin Acker (Revenant)\0"
-#define INFO_LEGAL  "Copyright 2014 by Revenant\0"
+#define INFO_LEGAL  "Copyright 2014-2015 by Revenant\0"
 
 #define INFO_DESC   "Kirby's Adventure level editor\0"
-#define INFO_VERS   "0.91\0"
-#define INFO_VNUM   0, 91, 0, 0
+#define INFO_VERS   "0.92\0"
+#define INFO_VNUM   0, 92, 0, 0
 #define INFO_NAME   "kale.exe\0"
 #define INFO_FILE   "kale.exe\0"
 
